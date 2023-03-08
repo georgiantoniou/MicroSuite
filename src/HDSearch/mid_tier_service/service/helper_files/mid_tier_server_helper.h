dbd116c878d787a83e6031b40d364eb762637462
Georgia Antoniou <63106778+georgiantoniou@users.noreply.github.com>
2023-03-08 12:16:27 +0000

    Update mid_tier_server_helper.h

--- src/HDSearch/mid_tier_service/service/helper_files/mid_tier_server_helper.h
+++ src/HDSearch/mid_tier_service/service/helper_files/mid_tier_server_helper.h
@@ -381,7 +381,9 @@ void MergeFromResponseMap(const std::vector<ResponseData> &response_data,
         uint64_t* calculate_knn_time,
         uint64_t* pack_bucket_resp_time,
         uint64_t* bucket_proc_time,
-        uint64_t* bucket_idle_time);
+        uint64_t* bucket_idle_time
+        std::vector<uint64_t>* start
+        std::vector<uint64_t>* end);
 
 /* Print a Matrix of points - represents a collection of 
    points with 'd' dimensions.
