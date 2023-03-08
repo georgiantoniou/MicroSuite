97973d369f85bb15c288e44886555d86baa1156f
Georgia Antoniou <63106778+georgiantoniou@users.noreply.github.com>
2023-03-08 10:47:39 +0000

    Update bucket_server.cc

--- src/HDSearch/bucket_service/service/bucket_server.cc
+++ src/HDSearch/bucket_service/service/bucket_server.cc
@@ -118,7 +118,7 @@ void ProcessRequest(NearestNeighborRequest &request,
     //GetCpuTimes(&idle_time_final, &total_time_final);
     
     //ganton12
-    uint64_t bucket_end = GetTimeInMicro()
+    uint64_t bucket_end = GetTimeInMicro();
     reply->mutable_timing_data_in_micro()->set_bucket_end_time(bucket_end);
    
     std::cout << bucket_start << " " << bucket_end << "\n";
