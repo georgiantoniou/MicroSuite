d15cd254a2fdc457d8f52525c96cfa6373cbaca9
Georgia Antoniou <63106778+georgiantoniou@users.noreply.github.com>
2023-04-25 15:55:19 +0100

    Update loadgen_union_client_helper.h

--- src/SetAlgebra/load_generator/helper_files/loadgen_union_client_helper.h
+++ src/SetAlgebra/load_generator/helper_files/loadgen_union_client_helper.h
@@ -30,6 +30,9 @@ struct TimingInfo
     uint64_t pack_union_resp_time = 0;
     uint64_t total_resp_time = 0;
     uint64_t union_time = 0;
+    //ganton12
+    std::vector<uint64_t> intersection_start_time;
+    std::vector<uint64_t> intersection_end_time;
 };
 
 /* This structure holds the data entered by the load generator user.*/
